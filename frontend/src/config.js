a83e737f38ffc4d855e9222cfe8396361e1f938c
hdcongo61-sudo <hdcongo61@gmail.com>
2025-10-04 17:23:56 +0100

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,5 @@
 // src/config.js
 export const API_URL = process.env.NODE_ENV === 'development' 
   ? 'http://localhost:5001/api'  // Backend URL
-  : '/api';  // Production URL
+
+  : 'https://ets-hd-home-decor.onrender.com';  // Production URL
