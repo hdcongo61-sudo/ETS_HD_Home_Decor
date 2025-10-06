6c4a12d6f55d367ddd081fe2cb5adf4d8e026101
hdcongo61-sudo <hdcongo61@gmail.com>
2025-10-06 12:33:39 +0100

    Update GlobalModals.js

--- frontend/src/components/GlobalModals.js
+++ frontend/src/components/GlobalModals.js
@@ -1,10 +1,16 @@
-// components/GlobalModals.js
-import React from 'react';
+import React, { useContext } from 'react';
 import GlobalSaleModal from './GlobalSaleModal';
 import GlobalPaymentModal from './GlobalPaymentModal';
 import FloatingActionButton from './FloatingActionButton';
+import AuthContext from '../context/AuthContext';
 
 const GlobalModals = () => {
+  const { auth } = useContext(AuthContext);
+
+  if (!auth?.isAuthenticated) {
+    return null;
+  }
+
   return (
     <>
       <FloatingActionButton />
